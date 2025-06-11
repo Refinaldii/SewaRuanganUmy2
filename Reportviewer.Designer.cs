a409179beb9d2cb625358ec51e4bebea2c79f28d
Refinaldii <refinaldi9786@gmail.com>
2025-06-11 20:30:15 +0700

    Final Part 3

--- Reportviewer.Designer.cs
+++ Reportviewer.Designer.cs
@@ -46,7 +46,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(95, 35);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "Kembali";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
